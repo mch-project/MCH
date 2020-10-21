c156202c19d9f732f872509bccec6d92fd2099b5
Etna <50696197+githubetna@users.noreply.github.com>
2020-10-21 08:10:36 -0500

    Add files via upload

---
+++ SPIesp.bat
@@ -0,0 +1,5 @@
+ECHO on
+REM Arranque del programa SPI para proceso interactivo
+REM SpiSdimch.exe -ap SPIesp.prm -DSN mch
+
+MCHCargaSPIfp -a HONDURAS_FbdR.csv
---
+++ generaSPI.bat
@@ -0,0 +1,16 @@
+rem Página web
+REM MCHWebLotep.exe -d mch -v "Precipitacion" -g HONDURAS -hd mchdwebprec.hdef -hs DiaPrecipitacion.html -fi HOY -ff HOY
+
+rem Mapa de Isolíneas
+EtisolMCHm2.exe -d mch -o HONDURASspi3 -rg -90/-82.5/11/18 -g HONDURAS -fi 1995/02/01 -ff 1995/02/01 -hf 8 -fp FEC -tdb 1 -I -3/3/0.5 -D 500 -dm SI -im 0.01 -Mp HONDURASDepCGL1r12:0 -Mb HONDURASDepCGL1r12 -t 2 -a 0.3 -Ed SI -Mo mexico -iA 20 -iC 10 -Ej 1.0/20/192/3 -E1 "Indice SPI a 3 meses #fecha " -E2 "." -E1p 1200/100/24 -E2p 1200/150/24 -Le 12 -i3 1750  -Clr2 "237/28/36/-3/255/192/0/-2/119/147/60/-1/145/208/78/0/145/208/78/1/0/114/195/2/0/33/99/300" -v IndiceSPI3
+
+rem Mapa de Isolíneas
+EtisolMCHm2.exe -d mch -o HONDURASspi6 -rg -90/-82.5/11/18 -g HONDURAS -fi 1995/02/01 -ff 1995/02/01 -hf 8 -fp FEC -tdb 1 -I -3/3/0.5 -D 500 -dm SI -im 0.01 -Mp HONDURASDepCGL1r12:0 -Mb HONDURASDepCGL1r12 -t 2 -a 0.3 -Ed SI -Mo mexico -iA 20 -iC 10 -Ej 1.0/20/192/3 -E1 "Indice SPI a 6 meses #fecha " -E2 "." -E1p 1200/100/24 -E2p 1200/150/24 -Le 12 -i3 1750  -Clr2 "237/28/36/-3/255/192/0/-2/119/147/60/-1/145/208/78/0/145/208/78/1/0/114/195/2/0/33/99/3" -v IndiceSPI6
+
+rem Mapa de Isolíneas
+EtisolMCHm2.exe -d mch -o HONDURASspi12 -rg -90/-82.5/11/18 -g HONDURAS -fi 1995/02/01 -ff 1995/02/01 -hf 8 -fp FEC -tdb 1 -I -3/3/0.5 -D 500 -dm SI -im 0.01 -Mp HONDURASDepCGL1r12:0 -Mb HONDURASDepCGL1r12 -t 2 -a 0.3 -Ed SI -Mo mexico -iA 20 -iC 10 -Ej 1.0/20/192/3 -E1 "Indice SPI a 12 meses #fecha " -E2 "." -E1p 1200/100/24 -E2p 1200/150/24 -Le 12 -i3 1750  -Clr2 "237/28/36/-3/255/192/0/-2/119/147/60/-1/145/208/78/0/145/208/78/1/0/114/195/2/0/33/99/3" -v IndiceSPI12
+
+rem Mapa de Isolíneas
+EtisolMCHm2.exe -d mch -o HONDURASspi24 -rg -90/-82.5/11/18 -g HONDURAS -fi 1995/02/01 -ff 1995/02/01 -hf 8 -fp FEC -tdb 1 -I -3/3/0.5 -D 500 -dm SI -im 0.01 -Mp HONDURASDepCGL1r12:0 -Mb HONDURASDepCGL1r12 -t 2 -a 0.3 -Ed SI -Mo mexico -iA 20 -iC 10 -Ej 1.0/20/192/3 -E1 "Indice SPI a 24 meses #fecha " -E2 "." -E1p 1200/100/24 -E2p 1200/150/24 -Le 12 -i3 1750  -Clr2 "237/28/36/-3/255/192/0/-2/119/147/60/-1/145/208/78/0/145/208/78/1/0/114/195/2/0/33/99/3" -v IndiceSPI24
+
+
